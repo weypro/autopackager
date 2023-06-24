bf8c825244d7e09938228789d531f5be664f5b85
wey <weypro@users.noreply.github.com>
2023-06-24 17:10:43 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,7 @@ struct Args {
 
 fn main() {
     tracing_subscriber::fmt()
-        // enable everything
         .with_max_level(tracing::Level::TRACE)
-        // sets this to be the default, global collector for this application.
         .init();
 
 
